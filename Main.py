3e52e24926d864a85b4d57383f9adb388947291a
himel99 <himel_miah99@yahoo.com>
2021-12-05 23:11:02 +0600

    scrapper and Main code with output csv file

--- Main.py
+++ Main.py
@@ -8,5 +8,8 @@ Created on Sat Dec  4 01:52:40 2021
 import glassdoor_scraper as gs
 import pandas as pd
 
+
 path = "F:/Python/ds_salary_proj/chromedriver"
 df = gs.get_jobs('Data Scientist',15,False, path, 15) 
+
+df.to_csv("glassdoor_jobs.csv")
---
+++ data_cleaning.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  5 21:11:29 2021
+
+@author: ASUS
+"""
+
