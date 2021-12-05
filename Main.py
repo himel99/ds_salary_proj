6a85a5db8c4e00adad92b6b7e7a96d90ec3bfb12
himel99 <himel_miah99@yahoo.com>
2021-12-05 20:22:55 +0600

    uploaded scraper and Main code to run

---
+++ Main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  4 01:52:40 2021
+
+@author: ASUS
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "F:/Python/ds_salary_proj/chromedriver"
+df = gs.get_jobs('Data Scientist',15,False, path, 15) 
